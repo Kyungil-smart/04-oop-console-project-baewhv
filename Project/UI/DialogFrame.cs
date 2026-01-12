fdf86f6f8b9a3a847f9cf304fcee4a4031f90386
baewhv <baewhv@naver.com>
2026-01-12 16:58:10 +0900

    [FEAT] 대화창 UI 추가

---
+++ Project/UI/DialogFrame.cs
@@ -0,0 +1,27 @@
+﻿namespace Project;
+
+public class DialogFrame
+{
+    private Vector _size;
+    private Vector _pos;
+    private Border _screen;
+    
+    
+    public Vector MaxSize => _size;
+
+    public DialogFrame(Vector pos, Vector size)
+    {
+        _pos = pos;
+        _size = size;
+        _screen = new Border(pos, size);
+    }
+    
+    public void DrawUI() //테두리는 1회만 그린다.
+    {
+        _screen.Draw();
+    }
+    public void Render()
+    {
+        
+    }
+}
