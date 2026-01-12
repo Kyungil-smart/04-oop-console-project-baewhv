7472209389560e7d6fd58d904f325f8efed710b2
baewhv <baewhv@naver.com>
2026-01-12 15:13:10 +0900

    [FEAT] 출력용 스크린 세팅 커밋

--- Project/UI/ScreenFrame.cs
+++ Project/UI/ScreenFrame.cs
@@ -4,10 +4,10 @@ namespace Project;
 
 public class ScreenFrame
 {
-    private Vector _size = new Vector(32, 18);
+    private Vector _size;
     private Vector _position = Vector.Zero;
     private Border _screenBorder;
-    private StringBuilder[] _screen;
+    private StringBuilder[,] _screen;
 
     private HealthUI _healthUi;
     public Vector GetSize => _size;
@@ -20,14 +20,15 @@ public class ScreenFrame
     {
         if (pos == null) pos = Vector.Zero;
         _screenBorder = new Border(pos, _size);
-
         _position = pos;
-        _screen = new StringBuilder[_size.Y];
+        _screen = new StringBuilder[_size.Y,_size.X];
         for (int i = 0; i < _size.Y; i++)
         {
-            _screen[i] = new StringBuilder("＃＆＆＆＆＆＆＆＆＆＆＆＆＆＆＆＆＆＆＆＆＆＆＆＆＆＆＆＆＆＆＃", _size.X / 2);
+            for (int j = 0; j < _size.X; j++)
+            {
+                _screen[i, j] = new StringBuilder("##",3);
+            }
         }
-
         _healthUi = new HealthUI();
         _healthUi.Init(player, new Vector(1, _size.Y + 1));
     }
@@ -41,13 +42,15 @@ public class ScreenFrame
 
     public void Render() //스크린 영역 랜더.
     {
-        for (int i = 0; i < _screen.Length; i++)
+        for (int i = 0; i < _screen.GetLength(0); i++)
         {
             Console.SetCursorPosition(_position.X + 2, _position.Y + 1 + i);
-            for (int j = 0; j < _screen[i].Length; j++)
+            for (int j = 0; j < _screen.GetLength(1); j++)
             {
-                
-                _screen[i][j].Print();
+                for (int k = 0; k < _screen[i, j].Length; k++)
+                {
+                    _screen[i,j][k].Print();    
+                }
             }
         }
     }
