7fa9a702e281b68ed2dff80c55ba0cf9518d130b
baewhv <baewhv@naver.com>
2026-01-12 11:00:13 +0900

    [FEAT] 테두리 색상을 설정할 수 있게 수정.

--- Project/UI/Border.cs
+++ Project/UI/Border.cs
@@ -6,6 +6,13 @@ public class Border
     public int Y;
     public int Width;
     public int Height;
+    public ConsoleColor Color { get; set; } = ConsoleColor.White;
+
+    public void DefaultColor()
+    {
+        Color = ConsoleColor.White;
+    }
+        
 
     public Border(int x = 0, int y = 0, int width = 2, int height = 2)
     {
@@ -13,6 +20,7 @@ public class Border
         Y = y;
         Width = width;
         Height = height;
+        
     }
 
     public void Draw()
@@ -22,25 +30,25 @@ public class Border
         Console.SetCursorPosition(X, Y);
         for (int i = 0; i < Width; i++)
         {
-            if (i == 0)  "┌─".Print();
-            else if( i == Width-1) "─┒".Print();
-            else "──".Print();
+            if (i == 0)  "┌─".Print(Color);
+            else if( i == Width-1) "─┒".Print(Color);
+            else "──".Print(Color);
         }
 
         for (int i = 1; i < Height - 1; i++)
         {
             Console.SetCursorPosition(X, Y+i);
-            "│".Print();
+            "│".Print(Color);
             Console.SetCursorPosition(X + (Width-1)*2, Y+i);
-            " ┃".Print();
+            " ┃".Print(Color);
         }
         
         Console.SetCursorPosition(X, Y+Height-1);
         for (int i = 0; i < Width; i++)
         {
-            if (i == 0)  "┕━".Print();
-            else if( i == Width-1) "━┛".Print();
-            else "━━".Print();
+            if (i == 0)  "┕━".Print(Color);
+            else if( i == Width-1) "━┛".Print(Color);
+            else "━━".Print(Color);
         }
     }
 }
