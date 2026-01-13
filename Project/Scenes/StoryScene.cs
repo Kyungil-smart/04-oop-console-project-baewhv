d174425a08a2ba5382e1e64d778c1f384c823798
baewhv <baewhv@naver.com>
2026-01-13 09:56:51 +0900

    [FIX] 누락된 커밋파일 업로드

--- Project/Scenes/StoryScene.cs
+++ Project/Scenes/StoryScene.cs
@@ -13,7 +13,7 @@ public class StoryScene : Scene
     {
         _player = player;
         _townMap = new TownMap(Vector.Zero);
-        _player._map = _townMap;
+        _player.CurrentMap = _townMap;
         _screenFrame = new ScreenFrame(_player);
         _dialogFrame = new DialogFrame(new Vector(0,_screenFrame.GetSize.Y+2), new Vector(_screenFrame.GetSize.X, 5));
 
